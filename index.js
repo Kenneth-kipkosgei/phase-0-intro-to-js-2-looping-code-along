d170dc734f826bf7ce35ce4fc99e9b1d6f1ec0be
kenneth <kennethkosgei38@gmail.com>
2025-06-19 10:52:33 +0300

    jslooping2code

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+// Function to create thank-you messages for each name
+function writeCards(names, eventName) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return messages;
+}
+
+// Function to count down from a given number to 0
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
